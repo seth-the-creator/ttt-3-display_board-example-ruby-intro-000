ec274ec36c550a15092ea3004fe832063ece3874
Seth Mueller <sethmueller1@gmail.com>
2020-07-10 04:16:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+<<<<<<< HEAD
 def display_board
   puts "   |   |   "
   puts "-----------"
@@ -6,3 +7,10 @@ def display_board
   puts "-----------"
   puts "   |   |   "
 end
+=======
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+>>>>>>> 7de9d39afbc0694702b0e8c253b4a497b8d3b7e1
